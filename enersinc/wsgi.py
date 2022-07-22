a8866a444bf2ebbc31366fdd3dec4c9fc563971c
JhonVB <jhonba2020@gmail.com>
2022-07-22 01:23:46 -0500

    back

--- enersinc/wsgi.py
+++ enersinc/wsgi.py
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 
 from django.core.wsgi import get_wsgi_application
 
-=======
+
 """
 WSGI config for enersinc project.
 
@@ -17,6 +17,4 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 from django.core.wsgi import get_wsgi_application
 
 
-
->>>>>>> 311a2a502f7fe1b8b72a7a081fcf49d64aebb43d
 application = get_wsgi_application()
